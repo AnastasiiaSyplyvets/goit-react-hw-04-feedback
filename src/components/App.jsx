8ec31150077d2fe2667d36ae6643a1d7aeb4c886
AnastasiiaSyplyvets <102216774+AnastasiiaSyplyvets@users.noreply.github.com>
2023-05-30 12:31:57 +0200

    2

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,70 @@
+import React from 'react';
+import Counter from './Statistic';
+import { FeedbackOptions } from './FeedbackOptions';
+import { Section } from './Section';
+import { Notification } from './message';
+import { useState } from 'react';
+
 export const App = () => {
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+
+  const handleAmount = event => {
+    const name = event.target.name;
+
+    switch (name) {
+      case 'good':
+        setGood(prevState => {
+          return prevState + 1;
+        });
+        break;
+      case 'neutral':
+        setNeutral(prevState => {
+          return prevState + 1;
+        });
+        break;
+      case 'bad':
+        setBad(prevState => {
+          return prevState + 1;
+        });
+        break;
+    }
+  };
+
+  const countTotalFeedback = () => {
+    let total = good + neutral + bad;
+
+    return total;
+  };
+
+  const countPositiveFeedbackPercentage = () => {
+    let posTotal = Math.round((good * 100) / countTotalFeedback());
+
+    return posTotal;
+  };
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <>
+      <Section>
+        <FeedbackOptions
+          options={['good', 'neutral', 'bad']}
+          onLeaveFeedback={handleAmount}
+        />
+      </Section>
+      <Section>
+        {countTotalFeedback() === 0 && (
+          <Notification message="No feedback given" />
+        )}
+        {countTotalFeedback() !== 0 && (
+          <Counter
+            good={good}
+            neutral={neutral}
+            bad={bad}
+            total={countTotalFeedback()}
+            positivePercentage={countPositiveFeedbackPercentage()}
+          />
+        )}
+      </Section>
+    </>
   );
 };
